71e8ff877bd3ee0db516465b10d3d647473cd98b
Vinícius Dos Santos <vinny.santosk8@gmail.com>
2022-09-29 16:56:05 -0300

    Backup Project

--- sitio/js/script.js
+++ sitio/js/script.js
@@ -1,2 +1,40 @@
 var x = document.getElementsByClassName("productslider");
 
+function clickDrop() {
+  document.getElementById("myDropdown").classList.toggle("showMenuSuspenso");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".myDropBtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("showMenuSuspenso")) {
+        openDropdown.classList.remove("showMenuSuspenso");
+      }
+    }
+  }
+};
+
+var imageSlider = [
+  "../image/slider/1.jpg",
+  "../image/slider/2.png",
+  "../image/slider/3.jpg",
+];
+var num = 0;
+function next() {
+  var slider = document.getElementById("slider");
+  num++;
+  if (num >= imageSlider.length) {
+    num = 0;
+  }
+  slider.src = imageSlider[num];
+}
+function prev() {
+  var slider = document.getElementById("slider");
+  num--;
+  if (num < 0) {
+    num = imageSlider.length - 1;
+  }
+  slider.src = imageSlider[num];
+}
