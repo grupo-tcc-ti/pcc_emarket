b97e6f32e43164728939d625d0af0e843064df7a
macaco-homem <macacohomem1337@gmail.com>
2022-10-04 16:06:18 -0300

    Update produtos.php

--- admin/produtos.php
+++ admin/produtos.php
@@ -25,8 +25,34 @@ if (!isset($admin_id)) {
     <title>Cadastro de Produtos</title>
 </head>
 <body>
+<?php include '../components/admin_header.php';?>
 
-
+<section class="add-produtos"></section>
+    <form action="" method="post" enctype="multipart/form-data">
+        <div class="flex">
+            <div class="inputBox">
+                <span required-field>Nome do Produto</span>
+                <input type="text" name="nome" required
+                palceholder="Insira o nome do produto" maxlength="100" class="box">
+            </div>
+            <div class="inputBox"></div>
+            <span required-field>Preço do Produto</span>
+            <input type="number" name="preco" class="box" required
+            placeholder="Insira o preço do produto" min="0" max="9999999999" onclick="if(this.value.length == 100) return false;">
+        </div>
+        <div class="inputBox">
+            <span required-field>Imagens</span>
+            <input type="file" name="image" class="box" required
+            accept="image/jpg, image/jpeg, image/png, image/webp" multiple>
+        </div>
+        <div class="inputBox">
+            <span required-field>Descrição do Produto</span>
+            <textarea name="descricao" class="box" cols="30" rows="10"required
+            placeholder="Insira as descrições do produto" maxlength="500"></textarea>
+        </div>
+        <input type="submit" value="Adicionar Produto" name="add_produto" class="btn">
+    </form>
+</section>
 
 <script src="../js/admin_script.js"></script>
 </body>
