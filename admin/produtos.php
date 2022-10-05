d2c9bc42f0e3824349f8e8e94c82abb38176544d
macaco-homem <macacohomem1337@gmail.com>
2022-10-05 17:39:59 -0300

    Update produtos.php

--- admin/produtos.php
+++ admin/produtos.php
@@ -11,12 +11,11 @@ $admin_id = $_SESSION['admin_id'];
 // $name = $my_form->name;
 // print_r($name);
 
-
 if (!isset($admin_id)) {
     header('location:../components/admin_header.php');
 }
 
-if (isset($_POST['add_produto'])){
+if (isset($_POST['add_produto'])) {
     $nome = $_POST['nome'];
     $nome = filter_var($nome, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $descricao = $_POST['descricao'];
@@ -33,13 +32,12 @@ if (isset($_POST['add_produto'])){
     if ($select_produtos->rowCount() > 0) {
         $message[] = 'Um produto com o mesmo nome ja existe!';
         $name_valid = false;
-    } 
-    else { $name_valid = true; }
+    } else { $name_valid = true;}
     //continua procedimento
     $image[] = array();
     $total_imgfiles = count($_FILES['image']['name']); //Contar o numero de arquivos na array
     //Loop através da array image
-    for( $i=0 ; $i < $total_imgfiles ; $i++ ) {
+    for ($i = 0; $i < $total_imgfiles; $i++) {
         $image[$i] = $_FILES['image']['name'][$i]; //O nome original do arquivo na máquina do cliente.
         // $image[$i] = array_filter($image[$i]);
         $image[$i] = filter_var($image[$i], FILTER_SANITIZE_FULL_SPECIAL_CHARS); //filtrar processo de arquivos e caracteres especiais
@@ -47,30 +45,29 @@ if (isset($_POST['add_produto'])){
         //tmp_name O nome temporário com o qual o arquivo enviado foi armazenado no servidor.
         $image_tmp = $_FILES['image']['tmp_name'][$i];
         //Verifica se caminho vazio
-        if ($image_tmp != ""){
+        if ($image_tmp != "") {
             //Configura novo caminho
-            $image_folder = "../uploaded_imgs/".$_FILES['image']['name'][$i];
-            //O arquivo e enviado ao caminho temporario 
-            if(move_uploaded_file($image_tmp, $image_folder)) { $path_valid = true; }
+            $image_folder = "../uploaded_imgs/" . $_FILES['image']['name'][$i];
+            //O arquivo e enviado ao caminho temporario
+            if (move_uploaded_file($image_tmp, $image_folder)) {$path_valid = true;}
         }
         //Verifica o tamanho do arquivo
-        $image_size = $_FILES['image']['size'][$i]; //O tamanho, em bytes, do arquivo enviado. 
-        if ($image_size > 2000000){
+        $image_size = $_FILES['image']['size'][$i]; //O tamanho, em bytes, do arquivo enviado.
+        if ($image_size > 2000000) {
             $message[] = 'Tamanho do arquivo é maior que o permitido!';
             $size_valid = false;
-        }else {$size_valid = true;}
+        } else { $size_valid = true;}
         //Confirma se imagens validas
     }
-    if ($name_valid && $path_valid && $size_valid){
-    //insere o produto
-    $inserir_produto = $conn->prepare("INSERT INTO `produtos` (nome, descricao, preco, image)
+    if ($name_valid && $path_valid && $size_valid) {
+        //insere o produto
+        $inserir_produto = $conn->prepare("INSERT INTO `produtos` (nome, descricao, preco, image)
     VALUES (?,?,?,?)");
-    $inserir_produto->execute([$nome, $descricao, $preco, join(',',$image)]);
-    $message[] = 'Produto adicionado com sucesso!';
+        $inserir_produto->execute([$nome, $descricao, $preco, join(',', $image)]);
+        $message[] = 'Produto adicionado com sucesso!';
     }
     // $image_tmp_folder = '../uploaded_imgs/'.$image; //old
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -123,22 +120,34 @@ if (isset($_POST['add_produto'])){
 
 <section class="mostrar-produtos">
     <div class="box-container">
-    <?php 
-            $mostrar_produtos = $conn->prepare("SELECT * FROM  `produtos`");
-            $mostrar_produtos->execute();
-            if ($mostrar_produtos->rowCount() > 0) {
-                while ($fetch_produtos = $mostrar_produtos->fetch(PDO::FETCH_ASSOC)){
-                    $fetched_imgs = explode(",", $fetch_produtos['image']);
-                    ?>
-                        <div class="box">
-                        <img src='../uploaded_imgs/<?=$fetched_imgs[0];?>' width=200px height=auto alt="">
-                        </div>
-                    <?php 
-                }
-            } else {
-                echo '<p class="vazio">Nenhum produto adicionado...</p>';
-            }
+    <?php
+    $mostrar_produtos = $conn->prepare("SELECT * FROM  `produtos`");
+    $mostrar_produtos->execute();
+    if ($mostrar_produtos->rowCount() > 0) {
+    while ($fetch_produto = $mostrar_produtos->fetch(PDO::FETCH_ASSOC)) {
+        $fetched_imgs = explode(",", $fetch_produto['image']);
         ?>
+            <div class="box">
+                <img src='../uploaded_imgs/<?=$fetched_imgs[0];?>' width=200px height=auto alt="">
+                <div class="name"><?=$fetch_produto['nome'];?></div>
+                <div class="preco"><?=$fetch_produto['preco'];?></div>
+                <div class="descricao"><?=$fetch_produto['descricao'];?></div>
+                <div class="flex-btn">
+                    <a href='alterar_produto.php?alterar=<?=$fetch_produto['id'];?>' class="option-btn">
+                    Alterar</a>
+                </div>
+                <div class="flex-btn">
+                    <a href='produto.php?deletar=<?=$fetch_produto['id'];?>' class="delete-btn"
+                    onclick="return confirm('Deseja excluir o produto?');">
+                    Deletar</a>
+                </div>
+            </div>
+        <?php
+        }
+    } else {
+    echo '<p class="vazio">Nenhum produto adicionado...</p>';
+    }
+    ?>
     </div>
 </section>
 
@@ -152,7 +161,7 @@ if (isset($_POST['add_produto'])){
 <script src="../js/admin_script.js"></script>
 </body>
 </html>
-<!-- https://www.amazon.com/LG-43NANO75UPA-Alexa-Built-NanoCell/dp/B08WHSH4JH/ref=sr_1_1?crid=2HCT7TB38NMWH&keywords=smart+tv&qid=1664969634&qu=eyJxc2MiOiI2Ljk3IiwicXNhIjoiNi42MCIsInFzcCI6IjUuOTUifQ%3D%3D&refinements=p_36%3A8589204011&rnid=8589203011&s=tv&sprefix=smart+t%2Caps%2C253&sr=1-1 
+<!-- https://www.amazon.com/LG-43NANO75UPA-Alexa-Built-NanoCell/dp/B08WHSH4JH/ref=sr_1_1?crid=2HCT7TB38NMWH&keywords=smart+tv&qid=1664969634&qu=eyJxc2MiOiI2Ljk3IiwicXNhIjoiNi42MCIsInFzcCI6IjUuOTUifQ%3D%3D&refinements=p_36%3A8589204011&rnid=8589203011&s=tv&sprefix=smart+t%2Caps%2C253&sr=1-1
     Visor nano 4k de verdade: dê vida aos seus programas favoritos com NanoCell vibrante. Veja a imagem natural e realista com Nano Color aprimorada por um bilhão de cores ricas. Dimensões da TV sem suporte (L x A x P)-96,8 x 56,3 x 5,8 cm
 Processador Quad Core 4K: Nosso processador Quad Core 4K oferece uma experiência de visualização suave e nítida com maior contraste, cor e preto. Fonte de alimentação (tensão, Hz): CA 100~240V 50-60Hz, consumo de energia: 55W
 Experiência em cinema doméstico: veja e sinta que você está em ação com Active HDR. Veja os filmes exatamente como os diretores pretendem com o Modo Filmmaker. E com acesso integrado aos canais Netflix, Prime Video, Apple TV Plus, Disney Plus e LG, seu conteúdo favorito está ao seu alcance.
