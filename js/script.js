c62deb5c0ee01abf2085a7093c0d3e62c71bc911
Dukk1 <dukksupercao@gmail.com>
2022-10-05 18:34:10 -0300

    update banner slide

--- js/script.js
+++ js/script.js
@@ -38,3 +38,85 @@ function prev() {
   }
   slider.src = imageSlider[num];
 }
+
+
+/* Functions para o banner: */
+const slider = document.querySelector(".slider"); //var slider dos banner
+const nextBtn = document.querySelector(".next-btn"); //var botao proximo
+const prevBtn = document.querySelector(".prev-btn"); //var botao anterior
+const slides = document.querySelectorAll(".slide"); //var cada imagem
+const slideIcons = document.querySelectorAll(".slide-icon"); //var icone dos botoes
+const numberOfSlides = slides.length;
+var slideNumber = 0;
+
+//next button
+nextBtn.addEventListener("click", () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  slideIcons.forEach((slideIcon) => {
+    slideIcon.classList.remove("active");
+  });
+
+  slideNumber++;
+
+  if(slideNumber > (numberOfSlides - 1)){
+    slideNumber = 0;
+  }
+
+  slides[slideNumber].classList.add("active");
+  slideIcons[slideNumber].classList.add("active");
+});
+
+//previous button
+prevBtn.addEventListener("click", () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  slideIcons.forEach((slideIcon) => {
+    slideIcon.classList.remove("active");
+  });
+
+  slideNumber--;
+
+  if(slideNumber < 0){
+    slideNumber = numberOfSlides - 1;
+  }
+
+  slides[slideNumber].classList.add("active");
+  slideIcons[slideNumber].classList.add("active");
+});
+
+//autoplay
+var playSlider;
+var autoPlayTransicao = () => {
+  playSlider = setInterval(function(){
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+    slideIcons.forEach((slideIcon) => {
+      slideIcon.classList.remove("active");
+    });
+
+    slideNumber++;
+
+    if(slideNumber > (numberOfSlides - 1)){
+      slideNumber = 0;
+    }
+
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+  }, 8000); //8 segundos == 8000 ms
+}
+autoPlayTransicao();
+
+//mouse em cima do banner, para o autoplay
+slider.addEventListener("mouseover", () => {
+  clearInterval(playSlider);
+});
+
+//mouse fora do banner, volta o autoplay
+slider.addEventListener("mouseout", () => {
+  autoPlayTransicao();
+});
+  
