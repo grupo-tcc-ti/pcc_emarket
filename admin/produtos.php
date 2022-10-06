98528ae73ff2374d06de8b279233a6aa5a5e2cd7
macaco-homem <macacohomem1337@gmail.com>
2022-10-06 19:28:23 -0300

    Update produtos

--- admin/produtos.php
+++ admin/produtos.php
@@ -15,6 +15,7 @@ if (!isset($admin_id)) {
     header('location:../components/admin_header.php');
 }
 
+// Cadastrar produto ###################################################################
 if (isset($_POST['add_produto'])) {
     $nome = $_POST['nome'];
     $nome = filter_var($nome, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -23,10 +24,11 @@ if (isset($_POST['add_produto'])) {
     $preco = $_POST['preco'];
     $preco = filter_var($preco, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-    $select_produtos = $conn->prepare("SELECT * FROM `produtos` WHERE nome = ?");
-    $select_produtos->execute([$nome]);
+    $select_produtos = $conn->prepare("SELECT * FROM `produtos` WHERE nome = :nome");
+    $select_produtos->bindParam(':nome', $nome);
+    $select_produtos->execute();
 
-    $name_valid = $path_valid = $size_valid = false;
+    // $name_valid = $path_valid = $size_valid = false;
 
     //Verifica se produto com mesmo nome
     if ($select_produtos->rowCount() > 0) {
@@ -34,40 +36,105 @@ if (isset($_POST['add_produto'])) {
         $name_valid = false;
     } else { $name_valid = true;}
     //continua procedimento
-    $image[] = array();
-    $total_imgfiles = count($_FILES['image']['name']); //Contar o numero de arquivos na array
-    //Loop através da array image
-    for ($i = 0; $i < $total_imgfiles; $i++) {
-        $image[$i] = $_FILES['image']['name'][$i]; //O nome original do arquivo na máquina do cliente.
-        // $image[$i] = array_filter($image[$i]);
-        $image[$i] = filter_var($image[$i], FILTER_SANITIZE_FULL_SPECIAL_CHARS); //filtrar processo de arquivos e caracteres especiais
-        //O caminho temporario do arquivos é armazenado
-        //tmp_name O nome temporário com o qual o arquivo enviado foi armazenado no servidor.
-        $image_tmp = $_FILES['image']['tmp_name'][$i];
-        //Verifica se caminho vazio
-        if ($image_tmp != "") {
-            //Configura novo caminho
-            $image_folder = "../uploaded_imgs/" . $_FILES['image']['name'][$i];
-            //O arquivo e enviado ao caminho temporario
-            if (move_uploaded_file($image_tmp, $image_folder)) {$path_valid = true;}
+    $image = array();
+    if ($_FILES['image']['error']==0){
+        $prod_path = "../image/produtos_teste/";
+        
+        if (is_array($_FILES['image']['name'])){
+            $total_imgfiles = count($_FILES['image']['name']); //Contar o numero de arquivos na array
+        //Loop através da array image
+            for ($i = 0; $i < $total_imgfiles; $i++) {
+                $image[$i] = $prod_path.$_FILES['image']['name'][$i]; //O nome original do arquivo na máquina do cliente.
+                // $image[$i] = array_filter($image[$i]);
+                $image[$i] = filter_var($image[$i], FILTER_SANITIZE_FULL_SPECIAL_CHARS); //filtrar processo de arquivos e caracteres especiais
+                //O caminho temporario do arquivos é armazenado
+                //tmp_name O nome temporário com o qual o arquivo enviado foi armazenado no servidor.
+                $image_tmp = $_FILES['image']['tmp_name'][$i];
+                //Verifica se caminho vazio
+                if ($image_tmp != "") {
+                    //Configura novo caminho
+                    $image_folder = $prod_path . $_FILES['image']['name'][$i];
+                    //O arquivo e enviado ao caminho temporario
+                    if (move_uploaded_file($image_tmp, $image_folder)) {$path_valid = true;}
+                }
+                //Verifica o tamanho do arquivo
+                $image_size = $_FILES['image']['size'][$i]; //O tamanho, em bytes, do arquivo enviado.
+                if ($image_size > 2000000) {
+                    $message[] = 'Tamanho do arquivo é maior que o permitido!';
+                    $size_valid = false;
+                } else { $size_valid = true;}
+            }
+        } else {
+            $image = $prod_path.$_FILES['image']['name'];
+            $image = filter_var($image, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $image_tmp = $_FILES['image']['tmp_name'];
+            if ($image_tmp != "") {
+                //Configura novo caminho
+                $image_folder = $prod_path . $_FILES['image']['name'];
+                //O arquivo e enviado ao caminho temporario
+                if (move_uploaded_file($image_tmp, $image_folder)) {$path_valid = true;}
+            }
+            //Verifica o tamanho do arquivo
+            $image_size = $_FILES['image']['size']; //O tamanho, em bytes, do arquivo enviado.
+            if ($image_size > 2000000) {
+                $message[] = 'Tamanho do arquivo é maior que o permitido!';
+                $size_valid = false;
+            } else { $size_valid = true;}
         }
-        //Verifica o tamanho do arquivo
-        $image_size = $_FILES['image']['size'][$i]; //O tamanho, em bytes, do arquivo enviado.
-        if ($image_size > 2000000) {
-            $message[] = 'Tamanho do arquivo é maior que o permitido!';
-            $size_valid = false;
-        } else { $size_valid = true;}
-        //Confirma se imagens validas
     }
+    
+    // else if ($_FILES['image']['error']==4){$path_valid = true; $size_valid = true;}
+    // $path_valid = true; $size_valid = true;
+    
+    //Confirma se imagens validas
     if ($name_valid && $path_valid && $size_valid) {
-        //insere o produto
+        //Insere o produto
         $inserir_produto = $conn->prepare("INSERT INTO `produtos` (nome, descricao, preco, image)
-    VALUES (?,?,?,?)");
-        $inserir_produto->execute([$nome, $descricao, $preco, join(',', $image)]);
+    VALUES (:nome, :descricao, :preco, :image)");
+        $inserir_produto->bindParam(":nome", $nome);
+        $inserir_produto->bindParam(":descricao", $descricao);
+        $inserir_produto->bindParam(":preco", $preco);
+        $inserir_produto->bindValue(":image", is_array($image)?implode(',', $image):$image);
+        $inserir_produto->execute();
         $message[] = 'Produto adicionado com sucesso!';
     }
-    // $image_tmp_folder = '../uploaded_imgs/'.$image; //old
 }
+
+// Deletar produto ###################################################################
+if(isset($_GET['delete_prod'])){
+    
+    $delete_id = $_GET['delete_prod'];
+    $del_prod_img = $conn->prepare("SELECT * FROM `produtos` WHERE codProduto = :id"); //_img
+    $del_prod_img->bindParam(':id', $delete_id);
+    $del_prod_img->execute();
+    /* Unlink image from product*/
+    $fetch_del_img = $del_prod_img->fetch(PDO::FETCH_ASSOC);
+    if ($fetch_del_img['image']) {
+        $fetched_imgs = explode(",", $fetch_del_img['image']);
+        foreach ($fetched_imgs as $prod_img){
+            // echo($prod_img);
+            // unlink($prod_img);
+            if(file_exists($prod_img)){
+                echo($prod_img.':image_unlinked');
+                // unlink($prod_img);
+            }else{echo($prod_img.':no');}
+        }
+    }
+    /* Delete produto db */
+    $del_prod = $conn->prepare("DELETE FROM `produtos` WHERE codProduto = :id");
+    $del_prod->bindParam(':id', $delete_id);
+    $del_prod->execute();
+
+    /* Delete produto cart and wishlist*/
+    $del_prod_cart_wlist = $conn->prepare("DELETE FROM `carrinho` WHERE :fk_id = :id");
+    $del_prod_cart_wlist = $conn->prepare("DELETE FROM `listadedesejo` WHERE :fk_id = :id");
+    $del_prod_cart_wlist->bindParam(':id', $delete_id);
+    $del_prod_cart_wlist->bindValue(':fk_id', 'produtos_codProduto');
+    $del_prod_cart_wlist->execute();
+    header('location: produtos.php');
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -104,8 +171,8 @@ if (isset($_POST['add_produto'])) {
             </div><br>
             <div class="inputbox">
                 <span class="prod-title required-field">Imagens</span>
-                <!-- <label for="input-file" class="btn">Enviar Imagens</label> -->
-                <input type="file" name="image[]" class="box" id="input-file" required
+                <!-- <label for="input-file" class="btn">Enviar Imagens</label> Implementar mais coisas depois-->
+                <input type="file" name="image" class="box" id="input-file" required
                 accept="image/jpg, image/jpeg, image/png, image/webp" multiple>
             </div><br>
             <div class="inputbox">
@@ -129,19 +196,27 @@ if (isset($_POST['add_produto'])) {
             $fetched_imgs = explode(",", $fetch_produto['image']);
             ?>
             <div class="box">
-                <img src='../uploaded_imgs/<?=$fetched_imgs[0];?>' alt="">
+                <img src='<?=$fetched_imgs[0];?>' alt="">
                 <div class="nome"><?=$fetch_produto['nome'];?></div>
                 <div class="preco">R$ <?=$fetch_produto['preco'];?></div>
                 <div class="descricao"><?=$fetch_produto['descricao'];?></div>
+                <form action="" method="get" name="prod_mgmt" class="prod_mgmt">
                 <div class="flex-btn">
-                    <a href='alterar_produto.php?alterar=<?=$fetch_produto['id'];?>' class="option-btn">
-                    Alterar</a>
+                    <button type="submit" name="update_prod" value="<?=$fetch_produto['codProduto'];?>" class="option-btn"
+                    onclick="return confirm('Você confirma as alterações?');"
+                    >Alterar</button>
                 </div>
                 <div class="flex-btn">
-                    <a href='produto.php?deletar=<?=$fetch_produto['id'];?>' class="delete-btn"
-                    onclick="return confirm('Deseja excluir o produto?');">
-                    Deletar</a>
+                    <!-- Aproach #1 -->
+                    <!-- <a href='produtos.php?delete_prod=<?=$fetch_produto['codProduto'];?>' class="delete-btn"
+                    onclick="return confirm('Deseja mesmo excluir o produto?');">
+                    Deletar</a> -->
+                    <!-- Aproach #2 -->
+                    <button type="submit" name="delete_prod" value="<?=$fetch_produto['codProduto'];?>" class="delete-btn"
+                    onclick="return confirm('Deseja mesmo excluir o produto?');"
+                    >Deletar</button>
                 </div>
+                </form>
             </div>
         <?php
         }
