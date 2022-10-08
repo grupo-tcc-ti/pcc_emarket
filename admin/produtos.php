909d148a7e19c7b5da2a5044f4ca6f5f53b27718
macaco-homem <macacohomem1337@gmail.com>
2022-10-08 11:01:43 -0300

    Patch array_img

--- admin/produtos.php
+++ admin/produtos.php
@@ -15,7 +15,7 @@ if (!isset($admin_id)) {
     header('location:../components/admin_header.php');
 }
 
-// Cadastrar produto ###################################################################
+// Adicionar produto starts ###################################################################
 if (isset($_POST['add_produto'])) {
     $nome = $_POST['nome'];
     $nome = filter_var($nome, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -28,69 +28,98 @@ if (isset($_POST['add_produto'])) {
     $select_produtos->bindParam(':nome', $nome);
     $select_produtos->execute();
 
-    // $name_valid = $path_valid = $size_valid = false;
+    $name_valid = $path_valid = $size_valid = $upload_valid = false;
 
     //Verifica se produto com mesmo nome
     if ($select_produtos->rowCount() > 0) {
         $message[] = 'Um produto com o mesmo nome ja existe!';
         $name_valid = false;
     } else { $name_valid = true;}
-    //continua procedimento
-    $image = array();
-    if ($_FILES['image']['error']==0){
-        $prod_path = "../image/produtos_teste/";
+
+
+    //Image handling procedure starts --------->
+    $fetched_imgs = $image = array();
+    $prod_path = "../image/produtos_teste/";
+    if (isset($_FILES['image'])){
+        $image_error = $_FILES['image']['error'];
+        foreach ($image_error as $image_error){
+            if ($image_error != 0) {
+            $upload_valid = false;
+            // echo 'upload_invalid:';
+            }
+            else {
+                $upload_valid = true;
+                // echo 'upload_valid:';
+            }
+        }
         
-        if (is_array($_FILES['image']['name'])){
-            $total_imgfiles = count($_FILES['image']['name']); //Contar o numero de arquivos na array
-        //Loop através da array image
-            for ($i = 0; $i < $total_imgfiles; $i++) {
-                $image[$i] = $prod_path.$_FILES['image']['name'][$i]; //O nome original do arquivo na máquina do cliente.
-                // $image[$i] = array_filter($image[$i]);
+        if (is_array($_FILES['image']['name']) && $upload_valid){
+            // echo ':isarray'."\n"; //debug
+            $files_img = array_filter($_FILES['image']);
+            // print_r($prod_path.$files_img['name'][1].':ispath'."\n"); //debug
+            //Loop através da array image
+            for ($i = 0; $i < count($files_img['name']); $i++) {
+                // echo ':isforeach        '."\n"; //debug
+                $image[$i] = $prod_path.$files_img['name'][$i]; //O nome original do arquivo na máquina do cliente.
                 $image[$i] = filter_var($image[$i], FILTER_SANITIZE_FULL_SPECIAL_CHARS); //filtrar processo de arquivos e caracteres especiais
                 //O caminho temporario do arquivos é armazenado
                 //tmp_name O nome temporário com o qual o arquivo enviado foi armazenado no servidor.
-                $image_tmp = $_FILES['image']['tmp_name'][$i];
+                $image_tmp = $files_img['tmp_name'][$i];
                 //Verifica se caminho vazio
                 if ($image_tmp != "") {
                     //Configura novo caminho
-                    $image_folder = $prod_path . $_FILES['image']['name'][$i];
+                    $image_folder = $prod_path . $files_img['name'][$i];
                     //O arquivo e enviado ao caminho temporario
-                    if (move_uploaded_file($image_tmp, $image_folder)) {$path_valid = true;}
+                    if (move_uploaded_file($image_tmp, $image_folder)) {
+                        $path_valid = true;
+                        // echo $size_valid.':arrayimg_path'."\n"; //debug
+                    }
                 }
                 //Verifica o tamanho do arquivo
-                $image_size = $_FILES['image']['size'][$i]; //O tamanho, em bytes, do arquivo enviado.
+                $image_size = $files_img['size'][$i]; //O tamanho, em bytes, do arquivo enviado.
                 if ($image_size > 2000000) {
                     $message[] = 'Tamanho do arquivo é maior que o permitido!';
                     $size_valid = false;
-                } else { $size_valid = true;}
+                } else { $size_valid = true; 
+                    // echo $size_valid.':arrayimg_size'."\n"; //debug
+                }
             }
-        } else {
-            $image = $prod_path.$_FILES['image']['name'];
+        } else if (!is_array($files_img['name']) && $upload_valid){
+            // echo ':isstring'."\n"; //debug
+            $image = $prod_path.$files_img['name'];
             $image = filter_var($image, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            $image_tmp = $_FILES['image']['tmp_name'];
+            $image_tmp = $files_img['tmp_name'];
             if ($image_tmp != "") {
                 //Configura novo caminho
-                $image_folder = $prod_path . $_FILES['image']['name'];
+                $image_folder = $prod_path . $files_img['name'];
                 //O arquivo e enviado ao caminho temporario
-                if (move_uploaded_file($image_tmp, $image_folder)) {$path_valid = true;}
+                if (move_uploaded_file($image_tmp, $image_folder)) {
+                    $path_valid = true;
+                    // echo $size_valid.':singleimg_path'."\n"; //debug
+                }
             }
             //Verifica o tamanho do arquivo
-            $image_size = $_FILES['image']['size']; //O tamanho, em bytes, do arquivo enviado.
+            $image_size = $files_img['size']; //O tamanho, em bytes, do arquivo enviado.
             if ($image_size > 2000000) {
                 $message[] = 'Tamanho do arquivo é maior que o permitido!';
                 $size_valid = false;
-            } else { $size_valid = true;}
+            } else { $size_valid = true;
+                // echo $size_valid.':singleimg_size'."\n"; //debug
+            }
         }
     }
-    
-    // else if ($_FILES['image']['error']==4){$path_valid = true; $size_valid = true;}
-    // $path_valid = true; $size_valid = true;
-    
+    //Image handling procedure ends--------->
+
+    // $path_valid = true; $size_valid = true;  //debug force true
+    // echo $name_valid.':last_name   '; //debug
+    // echo $path_valid.':last_path   '; //debug
+    // echo $size_valid.':last_size   '; //debug
+
     //Confirma se imagens validas
-    if ($name_valid && $path_valid && $size_valid) {
+    if ($name_valid && $path_valid && $size_valid && $upload_valid) {
         //Insere o produto
         $inserir_produto = $conn->prepare("INSERT INTO `produtos` (nome, descricao, preco, image)
-    VALUES (:nome, :descricao, :preco, :image)");
+        VALUES (:nome, :descricao, :preco, :image)");
         $inserir_produto->bindParam(":nome", $nome);
         $inserir_produto->bindParam(":descricao", $descricao);
         $inserir_produto->bindParam(":preco", $preco);
@@ -99,8 +128,9 @@ if (isset($_POST['add_produto'])) {
         $message[] = 'Produto adicionado com sucesso!';
     }
 }
+// Adicionar produto ends ###################################################################
 
-// Deletar produto ###################################################################
+// Deletar produto starts ###################################################################
 if(isset($_GET['delete_prod'])){
     
     $delete_id = $_GET['delete_prod'];
@@ -120,12 +150,12 @@ if(isset($_GET['delete_prod'])){
             }else{echo($prod_img.':no');}
         }
     }
-    /* Delete produto db */
+    /* Deleta produto do banco */
     $del_prod = $conn->prepare("DELETE FROM `produtos` WHERE codProduto = :id");
     $del_prod->bindParam(':id', $delete_id);
     $del_prod->execute();
 
-    /* Delete produto cart and wishlist*/
+    /* Deleta produto do carrinho e listadedesejo*/
     $del_prod_cart_wlist = $conn->prepare("DELETE FROM `carrinho` WHERE :fk_id = :id");
     $del_prod_cart_wlist = $conn->prepare("DELETE FROM `listadedesejo` WHERE :fk_id = :id");
     $del_prod_cart_wlist->bindParam(':id', $delete_id);
@@ -133,6 +163,90 @@ if(isset($_GET['delete_prod'])){
     $del_prod_cart_wlist->execute();
     header('location: produtos.php');
 }
+// Deletar produto ends ###################################################################
+
+function debugImgArr($img_name = ''){
+    $image = array();
+    if (UPLOAD_ERR_OK === $_FILES[$img_name]['error']){echo 'no errorok        ';}else{echo 'errorok        ';}
+    if ($_FILES[$img_name]['error'] == 0){echo 'no error0        ';}else{echo 'error0        ';}
+    if (isset($_FILES[$img_name]) && $_FILES[$img_name]["error"] == 0){
+        if (is_array($_FILES[$img_name]['name'])) {
+            $total_imgfiles = count($_FILES[$img_name]['name']); //Contar o numero de arquivos na array
+            echo $image.':is array with:'.$total_imgfiles.' imgfiles';
+        }
+    } else {
+    echo $image.':not array';
+    $image_name     = $_FILES[$img_name]["name"];
+    $image_type     = $_FILES[$img_name]["type"];
+    $image_size     = $_FILES[$img_name]["size"];
+    $image_tmp_name = $_FILES[$img_name]["tmp_name"];
+    $image_error = $_FILES[$img_name]["error"];
+    
+    echo "<div style='text-align: center; background: #4CB974;
+    padding: 30px 0 10px 0; font-size: 20px; color: #fff'>
+    File Name: " . $image_name . "</div>";
+    
+    echo "<div style='text-align: center; background: #4CB974;
+    padding: 10px; font-size: 20px; color: #fff'>
+    File Type: " . $image_type . "</div>";
+    
+    echo "<div style='text-align: center; background: #4CB974;
+    padding: 10px; font-size: 20px; color: #fff'>
+    File Size: " . $image_size . "</div>";
+    
+    echo "<div style='text-align: center; background: #4CB974;
+    padding: 10px; font-size: 20px; color: #fff'>
+    File Error: " . $image_error . "</div>";
+    
+    echo "<div style='text-align: center; background: #4CB974;
+    padding: 10px; font-size: 20px; color: #fff'>
+    File Temporary Name: " . $image_tmp_name . "</div>";
+    }
+}
+
+// if ($_FILES['file']['error'] === UPLOAD_ERR_OK) {
+// } else {
+// throw new UploadException($_FILES['file']['error']);
+// }
+class UploadException extends Exception
+{
+    public function __construct($code) {
+        $msg = $this->codeToMessage($code);
+        parent::__construct($msg, $code);
+    }
+
+    private function codeToMessage($code)
+    {
+        switch ($code) {
+            case UPLOAD_ERR_INI_SIZE:
+                $msg = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
+                break;
+            case UPLOAD_ERR_FORM_SIZE:
+                $msg = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+                break;
+            case UPLOAD_ERR_PARTIAL:
+                $msg = "The uploaded file was only partially uploaded";
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                $msg = "No file was uploaded";
+                break;
+            case UPLOAD_ERR_NO_TMP_DIR:
+                $msg = "Missing a temporary folder";
+                break;
+            case UPLOAD_ERR_CANT_WRITE:
+                $msg = "Failed to write file to disk";
+                break;
+            case UPLOAD_ERR_EXTENSION:
+                $msg = "File upload stopped by extension";
+                break;
+            default:
+                $msg = "Unknown upload error";
+                break;
+        }
+        return $msg;
+    }
+}
+
 
 
 ?>
@@ -159,7 +273,7 @@ if(isset($_GET['delete_prod'])){
             <div class="inputbox">
                 <span class="prod-title required-field">Nome do Produto</span>
                 <input type="text" name="nome" class="box" required
-                placeholder="Ex.: Placa de Vídeo Nvidia Geforce RTX..." maxlength="100">
+                placeholder="Ex.: Placa de Vídeo Nvidia Geforce RTX..." maxlength="200">
             </div><br>
             <div class="inputbox">
                 <span class="prod-title required-field">Preço do Produto</span>
@@ -169,7 +283,7 @@ if(isset($_GET['delete_prod'])){
             <div class="inputbox">
                 <span class="prod-title required-field">Imagens</span>
                 <!-- <label for="input-file" class="btn">Enviar Imagens</label> Implementar mais coisas depois-->
-                <input type="file" name="image" class="box" id="input-file" required
+                <input type="file" name="image[]" class="box" id="input-file" required
                 accept="image/jpg, image/jpeg, image/png, image/webp" multiple>
             </div><br>
             <div class="inputbox">
@@ -225,18 +339,10 @@ if(isset($_GET['delete_prod'])){
 </section>
 
 
-<!-- https://www.w3schools.com/PHP/func_string_join.asp -->
-<!-- https://www.educba.com/array-in-sql/ -->
-
-
-
-
 <script src="../js/admin_script.js"></script>
 </body>
 </html>
-<!-- https://www.amazon.com/LG-43NANO75UPA-Alexa-Built-NanoCell/dp/B08WHSH4JH/ref=sr_1_1?crid=2HCT7TB38NMWH&keywords=smart+tv&qid=1664969634&qu=eyJxc2MiOiI2Ljk3IiwicXNhIjoiNi42MCIsInFzcCI6IjUuOTUifQ%3D%3D&refinements=p_36%3A8589204011&rnid=8589203011&s=tv&sprefix=smart+t%2Caps%2C253&sr=1-1
-    Visor nano 4k de verdade: dê vida aos seus programas favoritos com NanoCell vibrante. Veja a imagem natural e realista com Nano Color aprimorada por um bilhão de cores ricas. Dimensões da TV sem suporte (L x A x P)-96,8 x 56,3 x 5,8 cm
-Processador Quad Core 4K: Nosso processador Quad Core 4K oferece uma experiência de visualização suave e nítida com maior contraste, cor e preto. Fonte de alimentação (tensão, Hz): CA 100~240V 50-60Hz, consumo de energia: 55W
-Experiência em cinema doméstico: veja e sinta que você está em ação com Active HDR. Veja os filmes exatamente como os diretores pretendem com o Modo Filmmaker. E com acesso integrado aos canais Netflix, Prime Video, Apple TV Plus, Disney Plus e LG, seu conteúdo favorito está ao seu alcance.
-Melhor jogo: experimente jogos em NanoCell. O Game Optimizer dá a você acesso mais fácil a todas as suas configurações de jogo e você terá o modo automático de baixa latência mais HGiG para uma imagem detalhada do jogo.
-Auxiliar do Google e Alex embutido: não há necessidade de um dispositivo extra – basta pedir música, clima, notícias, sua lista de compras da Amazon e muito mais. Além disso, controle convenientemente sua casa conectada e dispositivos inteligentes. -->
+<!-- 
+    https://www.amazon.com/LG-43NANO75UPA-Alexa-Built-NanoCell/dp/B08WHSH4JH/ref=sr_1_1?crid=2HCT7TB38NMWH&keywords=smart+tv&qid=1664969634&qu=eyJxc2MiOiI2Ljk3IiwicXNhIjoiNi42MCIsInFzcCI6IjUuOTUifQ%3D%3D&refinements=p_36%3A8589204011&rnid=8589203011&s=tv&sprefix=smart+t%2Caps%2C253&sr=1-1
+    https://www.amazon.com/-/pt/dp/B096BK7W5M/ref=sr_1_6?keywords=smartwatch&qid=1665190856&qu=eyJxc2MiOiI2LjY1IiwicXNhIjoiNS44MSIsInFzcCI6IjQuNTMifQ%3D%3D&refinements=p_36%3A1253506011&rnid=386442011&s=electronics&sprefix=smat%2Celectronics%2C332&sr=1-6
+-->
