7626b8b50d08ba652d7c8ec3513f26dd882bb0f3
macaco-homem <macacohomem1337@gmail.com>
2022-10-17 21:47:09 -0300

    connect_fix

--- components/connect.php
+++ components/connect.php
@@ -1,9 +1,7 @@
 <?php
 $db_name = 'mysql:host=localhost; dbname=emarket';
-// $user_name = 'gabs';
-// $user_password = '92513124Gabe@';
 $user_name = 'root';
 $user_password = '';
 
-$conn = new PDO($db_name, $user_name, $user_password)
+$conn = new PDO($db_name, $user_name, $user_password);
 ?>
